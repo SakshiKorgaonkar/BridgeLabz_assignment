6747d6a0e57908aa077190253ee37d17f9c67859
SakshiKorgaonkar <korgaonkarsakshi23@gmail.com>
2024-04-19 18:35:43 +0530

    [sakshi] add : uc1 done

---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+internal class LineComparison
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("welcome to line comparision computation program");
+        Console.WriteLine("Enter first point coordinates");
+        int x1 = Convert.ToInt32(Console.ReadLine());
+        int y1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second point coordinates");
+        int x2 = Convert.ToInt32(Console.ReadLine());
+        int y2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Length of line is");
+        double z = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+        Console.WriteLine(z);
+    }
+}
